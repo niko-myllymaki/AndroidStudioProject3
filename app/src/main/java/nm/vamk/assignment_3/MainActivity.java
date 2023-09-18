6b7c2c1185c35d8fb967e8299fb7db351dbe685a
Niko Myllymäki <niko.myllymaki1@gmail.com>
2023-09-18 20:59:20 +0300

    Added different logos for portrait and landscape view, scroll view, dimensions.xml.

--- app/src/main/java/nm/vamk/assignment_3/MainActivity.java
+++ app/src/main/java/nm/vamk/assignment_3/MainActivity.java
@@ -2,7 +2,10 @@ package nm.vamk.assignment_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setInitialImage();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        ImageView newImageView = findViewById(R.id.companyLogoPortrait);
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            //Toast.makeText(this, "Landscape orientation", Toast.LENGTH_SHORT).show();
+            newImageView.setImageResource(R.drawable.logo_2);
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            //Toast.makeText(this, "Portrait orientation", Toast.LENGTH_SHORT).show();
+            newImageView.setImageResource(R.drawable.logo_1);
+
+        }
+    }
+
+    public void setInitialImage() {
+        ImageView initialImageView = findViewById(R.id.companyLogoPortrait);
+        initialImageView.setImageResource(R.drawable.logo_1);
     }
 }
