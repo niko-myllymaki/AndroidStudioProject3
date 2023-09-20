db010c12909e50aed9a8ab09f7713ad01db54c9e
Niko Myllymäki <niko.myllymaki1@gmail.com>
2023-09-20 07:18:23 +0300

    Added toast when the orientation changes and possibilty to lock orientation.

--- app/src/main/java/nm/vamk/assignment_3/MainActivity.java
+++ app/src/main/java/nm/vamk/assignment_3/MainActivity.java
@@ -2,16 +2,22 @@ package nm.vamk.assignment_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
         setInitialImage();
     }
@@ -20,13 +26,15 @@ public class MainActivity extends AppCompatActivity {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         ImageView newImageView = findViewById(R.id.companyLogoPortrait);
+        String date = getCurrentDateTime();
+
         // Checks the orientation of the screen
         if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            //Toast.makeText(this, "Landscape orientation", Toast.LENGTH_SHORT).show();
             newImageView.setImageResource(R.drawable.logo_2);
+            Toast.makeText(this, date, Toast.LENGTH_SHORT).show();
         } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
-            //Toast.makeText(this, "Portrait orientation", Toast.LENGTH_SHORT).show();
             newImageView.setImageResource(R.drawable.logo_1);
+            Toast.makeText(this, date, Toast.LENGTH_SHORT).show();
 
         }
     }
@@ -35,4 +43,12 @@ public class MainActivity extends AppCompatActivity {
         ImageView initialImageView = findViewById(R.id.companyLogoPortrait);
         initialImageView.setImageResource(R.drawable.logo_1);
     }
+
+    public String getCurrentDateTime() {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+
+
 }
