740d3654de2d6cfab0b9245f7e6ddae527658ed1
Niko Myllymäki <niko.myllymaki1@gmail.com>
2023-09-21 21:01:14 +0300

    Random number generation and date time.

--- app/src/main/java/nm/vamk/assignment_3/MainActivity.java
+++ app/src/main/java/nm/vamk/assignment_3/MainActivity.java
@@ -5,63 +5,89 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
+import android.widget.TextView;
+import android.os.Handler;
+
+import org.w3c.dom.Text;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewRandomNumber;
+    private int randomNumber;
+    private Runnable randomNumberUpdate;
+    private Random random = new Random();
+    private Handler handler = new Handler();
+    private String dateTimeForToast = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
-        //setInitialImage();
+
+        textViewRandomNumber = findViewById(R.id.tw_random_number);
+        randomNumberUpdate = new Runnable() {
+            @Override
+            public void run() {
+
+                randomNumber = random.nextInt(100) + 1;
+                textViewRandomNumber.setText(getString(R.string.random_number, randomNumber));
+                //Run again after 1 second
+                handler.postDelayed(this, 1000);
+            }
+        };
+        // Start the initial update
+        handler.post(randomNumberUpdate);
     }
-/*
+
+
+
     @Override
-    public Object onRetainNonConfigurationInstance() {
-        //Here we can save the state we want
-        return "State was saved!";
+    public void onSaveInstanceState(Bundle outState) {
+        Log.d("debug", "At onSaveInstanceState");
+        String savedDateTime = getCurrentDateTime();
+        outState.putString("savedDateTime", savedDateTime);
+        super.onSaveInstanceState(outState);
+
     }
 
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        String currentValue = savedInstanceState.getString("currentValue");
+        String dateTimeOfLastOrientation = savedInstanceState.getString("savedDateTime");
+        dateTimeForToast = dateTimeOfLastOrientation;
+        Log.d("Date Time", dateTimeForToast);
     }
-*/
+
+
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        ImageView newImageView = findViewById(R.id.companyLogoPortrait);
-        String date = getCurrentDateTime();
-
         // Checks the orientation of the screen
         if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            //newImageView.setImageResource(R.drawable.logo_2);
-            Toast.makeText(this, date, Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, dateTimeForToast, Toast.LENGTH_SHORT).show();
         } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
-            //newImageView.setImageResource(R.drawable.logo_1);
-            Toast.makeText(this, date, Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, dateTimeForToast, Toast.LENGTH_SHORT).show();
 
         }
     }
 
-    /*
-    public void setInitialImage() {
-        ImageView initialImageView = findViewById(R.id.companyLogoPortrait);
-        initialImageView.setImageResource(R.drawable.logo_1);
-    }
-*/
+
     public String getCurrentDateTime() {
         DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         Date date = new Date();
         return dateFormat.format(date);
     }
+
 
 
 }
